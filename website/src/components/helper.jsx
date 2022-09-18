eabe7eaa62674e6e6c6ad113ce1c94ff0641f431
Peidong He <38892121+Thomasbulls@users.noreply.github.com>
2022-09-18 15:35:26 -0400

    update

--- website/src/components/helper.jsx
+++ website/src/components/helper.jsx
@@ -42,20 +42,20 @@ export const testimonials_reviews = [
         portrait: Portrait1,
         name: "Chen Deng",
         job_title: "Co-founder, LAMOSE",
-        review: "lorem........"
+        review: `" His interpersonal and communication skills have allowed him to develop productive working relationships with both our clients and our staffs... "`
     },
     {
         id: 2,
         portrait: Portrait2,
         name: "Lihang Ying",
         job_title: "Senior Computer Vision Engineer, Peloton Interactive",
-        review: "lorem........"
+        review: `" I have known Peidong for many years since he was a junior college student. I have been consistently impressed by his positive attitude whenever he encountered problems. His resilience makes me believe that he will be able to tackle any obstacles in his life... "`
     },
     {
         id: 3,
         portrait: Portrait3,
         name: "Junchao Wang",
         job_title: "Morgan Stanley Developer (Associate)",
-        review: "lorem........"
+        review: `" I can feel that he really wants to be a good developer. As one of his best friend, I have seen him failed in the past, but he stood up from failure every single time. He truly has a strong mentality...  "`
     }
 ]
